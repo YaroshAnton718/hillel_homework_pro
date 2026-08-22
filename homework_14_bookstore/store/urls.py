23b2beb5dcf2bb5df4aa4809abcb8ba5da20dce1
anton <antonyarosh139@gmail.com>
2026-08-22 17:54:53 +0300

    solved homework_16_1

--- homework_14_bookstore/store/urls.py
+++ homework_14_bookstore/store/urls.py
@@ -5,24 +5,36 @@ from .views import (
     BookDetailView,
     BookCreateView,
     BookUpdateView,
-    BookDeleteView
+    BookDeleteView,
+    RegisterView,
+    BookLoginView,
+    BookLogoutView,
 )
 
 app_name = 'store'
 
 urlpatterns = [
-    path('', BookListView.as_view(), name='book_list'
+    path('', BookListView.as_view(), name='book_list'),
+
+    path('register/', RegisterView.as_view(), name='register'),
+
+    path('login/', BookLoginView.as_view(), name='login'),
+
+    path('logout/', BookLogoutView.as_view(), name='logout'),
+
+    path('book/<int:pk>/', BookDetailView.as_view(), name='book_detail'),
+
+    path('book/create/', BookCreateView.as_view(), name='book_create'),
+
+    path(
+        'book/<int:pk>/update/',
+        BookUpdateView.as_view(),
+        name='book_update'
     ),
 
-    path('book/<int:pk>/', BookDetailView.as_view(), name='book_detail'
-    ),
-
-    path('book/create/', BookCreateView.as_view(), name='book_create'
-    ),
-
-    path('book/<int:pk>/update/', BookUpdateView.as_view(), name='book_update'
-    ),
-
-    path('book/<int:pk>/delete/', BookDeleteView.as_view(), name='book_delete'
+    path(
+        'book/<int:pk>/delete/',
+        BookDeleteView.as_view(),
+        name='book_delete'
     ),
 ]
